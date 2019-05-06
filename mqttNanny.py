59431516b9ca20e9d94b31ffce80f848ea5bba7d
Adrian Popa <adrian.popa.gh@gmail.com>
2019-05-06 15:01:36 +0300

    Initial implementation

--- mqttNanny.py
+++ mqttNanny.py
@@ -1,20 +1,45 @@
-import linuxControl
+import linuxControl as computer
 import paho.mqtt.client as mqtt
 import yaml
 import os
+import threading
+import time
+import logging
+from logging.config import dictConfig
+
+logging_config = dict(
+    version = 1,
+    formatters = {
+        'f': {'format':
+              '%(asctime)s %(levelname)-8s [%(funcName)s:%(lineno)d] %(message)s'}
+    },
+    handlers = {
+        'c': {'class': 'logging.StreamHandler',
+              'formatter': 'f',
+              'level': logging.DEBUG,
+              'stream': "ext://sys.stdout" }
+    },
+    root = {
+        'handlers': ['c'],
+        'level': logging.DEBUG,
+    },
+)
+
+dictConfig(logging_config)
+logger = logging.getLogger(__name__)
+
 
 # Prerequisites:
 # * pip: sudo apt-get install python-pip
-# * paho-mqtt: pip install paho-mqtt
-# * python-yaml: sudo apt-get install python-yaml
+# * paho-mqtt: sudo pip3 install paho-mqtt
+# * python-yaml: sudo apt-get install python3-yaml
 # * notify2: sudo apt-get install python3-notify2
 #sudo apt-get install espeak
 #sudo apt-get install xscreensaver
 #sudo apt-get install xdotool
 #sudo apt-get install imagemagick
 
-# Configuration file goes in /etc/mqttNanny.yaml and should contain something like:
-# TODO
+# Configuration file goes in /etc/mqttNanny.yaml and should be similar to the example provided
 
 # For startup copy mqttNanny.service to /etc/systemd/system/
 # Startup is done via systemd with
@@ -34,38 +59,158 @@ def parseConfig():
         try:
             conf = yaml.load(stream)
         except yaml.YAMLError as exc:
-            print(exc)
-            print("Unable to parse configuration file /etc/mqttNanny.yaml")
+            logger.error(exc)
+            logger.error("Unable to parse configuration file /etc/mqttNanny.yaml")
             sys.exit(1)
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
+    logger.info("Connected with result code " + str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    (result, mid) = client.subscribe(conf['command_topic'])
-    print("Got subscription result for " + conf['command_topic'] + ":" + str(result))
+    for user in conf['users']:
+        topic = conf['baseTopic']+user+'/'+conf['mqttTimeTopicSuffix']
+        (result, mid) = client.subscribe(topic)
+        logger.info("Got subscription result for " + topic + ":" + str(result))
+
+    #subscribe to the screenshotCommand topic
+    (result, mid) = client.subscribe(conf['mqttScreenshotCommand'])
+    logger.info("Got subscription result for " + conf['mqttScreenshotCommand'] + ":" + str(result))
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print("Received command:" + msg.topic + " " + str(msg.payload))
+    logger.debug("Received command:" + msg.topic + " " + str(msg.payload))
 
     # listen to requests, process them
     # set the replies over mqtt
-    if msg.topic == conf['test']:
-        if msg.payload == 'test':
+    timeTopics = {}
+    for user in conf['users']:
+        topic = conf['baseTopic']+user+'/'+conf['mqttTimeTopicSuffix']
+        timeTopics[topic]=user
+    	
+    if msg.topic == conf['mqttScreenshotCommand']:
+        if msg.payload == 'enable':
+            # start a thread that will periodically post back screenshots for a set interval of time. At the end, it disables itself            
             pass
+        if msg.payload == 'disable':
+            # kill the screenshot thread if it exists
+        	pass
+    if msg.topic in timeTopics.keys():
+        #msg.payload should be the number of minutes left
+        if t[timeTopics[msg.topic]] != int(msg.payload):
+            t[timeTopics[msg.topic]] = int(msg.payload)
+            if t[timeTopics[msg.topic]] > 0:
+                #might have been disabled, restore the user
+                tty = computer.getCurrentDisplay()
+                #get active user
+                try:
+                   (activeUser, display) = computer.getUserForDisplay(tty)
+                   computer.enableUser(activeUser)
+                except Exception as e:
+                    logger.warning(e)
+
 
 """ Initialize the MQTT object and connect to the server """
 parseConfig()
+#Load default time for the monitored users
+t={}
+for user in conf['users']:
+    t[user]=conf['users'][user]['defaultOfflineTime']
+    logger.info("Loaded {} default minutes for {}".format(str(t[user]), user))
+    # also, enable user (might have been disabled)
+    tty = computer.getCurrentDisplay()
+    # get active user
+    try:
+        (activeUser, display) = computer.getUserForDisplay(tty)
+        computer.enableUser(activeUser)
+    except Exception as e:
+        logger.warning(e)
+
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 if conf['mqttUser'] and conf['mqttPass']:
     client.username_pw_set(username=conf['mqttUser'], password=conf['mqttPass'])
 client.connect(conf['mqttServer'], conf['mqttPort'], 60)
+
+oldTTY=None
+oldDisplay=None
+oldActiveUser=None
+oldScreensaver=None
+oldApplication=None
+
+"""	Main processing loop. Keep track of time for each user """
+
+while True:
+    #get active display
+    tty=computer.getCurrentDisplay()
+    if tty != oldTTY:
+        #update the new value via mqtt
+        oldTTY = tty
+        if client:
+            client.publish(conf['baseTopic']+'tty', tty, 0, False)
+    #get active user
+    try:
+        (activeUser, display) = computer.getUserForDisplay(tty)
+        if activeUser != oldActiveUser:
+            oldActiveUser = activeUser
+            if client:
+                client.publish(conf['baseTopic']+'activeUser', activeUser, 0, False)
+        if display != oldDisplay:
+            oldDisplay = display
+            if client:
+                client.publish(conf['baseTopic']+'display', display, 0, False)
+        #is screensaver active?
+        screensaver = computer.isScreensaverOn(display)
+        if screensaver != oldScreensaver:
+            oldScreensaver = screensaver
+            if client:
+                client.publish(conf['baseTopic']+'screensaver', screensaver, 0, False)
+        #current application name
+        application = computer.getActiveWindowName(display)
+        if application != oldApplication:
+            oldApplication = application
+            if client:
+                client.publish(conf['baseTopic']+'application', application, 0, False)
+		
+        # Check if the current user still has time allowed. Active screensaver does not consume time
+        if not screensaver:
+            t[activeUser] = int(t[activeUser] - conf['checkInterval']/60.0)
+            logger.info("Tick down time for {}. Time left: {} min".format(activeUser, t[activeUser]))
+            if t[activeUser] == 10:
+                # 10 minutes left
+                computer.notify(10, display)
+            if t[activeUser] == 5:
+                # 5 minutes left
+                computer.notify(5, display)
+            if t[activeUser] == 1:
+                # final warning
+                computer.notify(1, display)
+            if t[activeUser] <= 0:
+                computer.notify(0, display)
+                time.sleep(4)
+                # lock screensaver and disable user
+                computer.disableUser(activeUser)
+                computer.lockScreensaver(display)
+            if client:
+                client.publish(conf['baseTopic']+activeUser+'/'+conf['mqttTimeTopicSuffix'], t[activeUser], 0, False)
+
+    except Exception as e:
+        logger.warning(e)
+        #invalidate the mqtt topics
+        if client:
+            client.publish(conf['baseTopic']+'activeUser', 'None', 0, False)
+            client.publish(conf['baseTopic']+'display', 'None', 0, False)
+            client.publish(conf['baseTopic']+'screensaver', False, 0, False)
+            client.publish(conf['baseTopic']+'application', 'None', 0, False)
+
+    if client:
+        logger.debug("Processing mqtt messages")
+        client.loop()
+
+    time.sleep(int(conf['checkInterval']))
+
 #listen for messages and call on_message when needed
-print("Listen to MQTT messages...")
-sys.stdout.flush()
-client.loop_forever()
+#logger.debug("Listen to MQTT messages...\n")
+#client.loop_forever()
